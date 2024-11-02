6c6a4564358189799e0778c74e4989404c64421b
Karol Krizan <karolkrizan123@gmail.com>
2024-11-02 21:52:22 +0100

    made progress towards roulette board, starting to become laggy - will need to start thinking about efficiency

--- testing.py
+++ testing.py
@@ -4,18 +4,30 @@ import numpy as np
 SCREEN_W = 800
 SCREEN_H = 800
 
-def pixels(centre_coords: tuple , r1: float, r2: float, alpha: float):
+def pixels(r1: float, r2: float, alpha: float):
+    angles_list = []
+
     constant =  np.pi *(2/37)
-    diameters = np.linspace(r1, r2 + 0.1, 200)
-    angles = np.linspace(alpha,alpha  + constant, 100)
-    angles_new = np.linspace(alpha + 2* constant, alpha + 3 * constant, 100 )
-    angles = np.append(angles,angles_new)
+    diameters = np.linspace(r1, r2 + 0.1, 40)
+    
+    
+    
+
+    for num in range(0, 36, 2):
+        angles_new = np.linspace(alpha + num* constant, alpha + (num + 1) * constant, 40 )
+        angles_list.append(angles_new)
+
+    angles = np.concatenate(angles_list)
     xv,yv = np.meshgrid(diameters, angles)
 
-    #plt.plot(xv, yv, marker='o', color='k', linestyle='none')
+    return xv, yv
+    
 
-    x_rot = centre_coords[0] +  xv * np.cos(yv)
-    y_rot = centre_coords[1] +  xv * np.sin(yv)
+def rotate(r: float,theta: float, centre_coords: tuple, angle: float):
+    
+
+    x_rot = centre_coords[0] +  r * np.cos(theta + angle)
+    y_rot = centre_coords[1] +  r * np.sin(theta + angle)
 
     return x_rot, y_rot
 
@@ -62,9 +74,15 @@ dot = pygame.Rect((300.16, 250.17, 1.5, 1))
 
 t = 0
 
+center_coords = (SCREEN_W / 2, (SCREEN_H / 8) * 3)
+
+r_coords, theta_coords = pixels( 140 , 180 ,  0)
+
 while run:
     
-    x_coords, y_coords = pixels((SCREEN_W / 2, (SCREEN_H / 8) * 3), 140 , 180 , 0 + t * 0.08)
+    x_co, y_co = rotate(r_coords,theta_coords,(SCREEN_W / 2, (SCREEN_H / 8) * 3), t * 0.08)
+
+    # r_coords, theta_coords = pixels((SCREEN_W / 2, (SCREEN_H / 8) * 3), 140 , 180 , 0 + t * 0.08)
     
     screen.fill((a,b,c))
 
@@ -78,7 +96,7 @@ while run:
     
     # pygame.draw.circle(screen , (240,50,50), (350,350), r)
     
-    draw(x_coords,y_coords)
+    draw(x_co,y_co)
     if pygame.key.get_pressed()[pygame.K_a] == True:
         t += 1
 
